a69cdfa8311af5854aab2accc7110dd875c26913
Kaucher Rakib <102960310+KaucherRakib@users.noreply.github.com>
2022-06-23 19:23:32 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(function(){
+
+// upcomming slider//
+    $('.up_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow:'.prev_arrow',
+        nextArrow:'.next_arrow',
+        Padding: '0px',
+      });
+   
+
+// upcomming slider//
+$('.events_slider_tow').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:'.prev_arrow_tow',
+  nextArrow:'.next_arrow_tow',
+  Padding: '0px',
+});
+
+$('.applicant_slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:'.prev_arrow_tow',
+  nextArrow:'.next_arrow_tow',
+  Padding: '0px',
+  margin: '0px',
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
